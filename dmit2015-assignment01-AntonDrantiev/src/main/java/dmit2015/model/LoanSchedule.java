492ac4127edb751baa1add98b74d3a48bafbae7d
dmit2015 <dmit2015@ubuntu>
2018-09-19 16:50:32 -0600

    Added two classes Loan and LoanSchedule for first class assignment

---
+++ dmit2015-assignment01-AntonDrantiev/src/main/java/dmit2015/model/LoanSchedule.java
@@ -0,0 +1,78 @@
+package dmit2015.model;
+
+public class LoanSchedule {
+	private int paymentNumber;
+	private double interestPaid;
+	private double principalPaid;
+	private double remainingBalance;
+	
+	public LoanSchedule() {
+		super();
+		this.paymentNumber = 1;
+		this.principalPaid = 0;
+		this.remainingBalance = 0;
+		this.interestPaid = 0;
+		
+	}
+	
+	public LoanSchedule(int paymentNumber, double interestPaid, double principalPaid, double remainingBalance) {
+		super();
+		this.paymentNumber = paymentNumber;
+		this.interestPaid = interestPaid;
+		this.principalPaid = principalPaid;
+		this.remainingBalance = remainingBalance;
+	}
+
+
+	@Override
+	public String toString() {
+		return "LoanSchedule [paymentNumber=" + paymentNumber + ", interestPaid=" + interestPaid + ", principalPaid="
+				+ principalPaid + ", remainingBalance=" + remainingBalance + "]";
+	}
+
+	public int getPaymentNumber() {
+		return paymentNumber;
+	}
+	public void setPaymentNumber(int paymentNumber) {
+		if(paymentNumber >= 0) {
+			this.paymentNumber = paymentNumber;
+		}
+	}
+		
+	
+	public double getInterestPaid() {
+		return interestPaid;
+	}
+	public void setInterestPaid(double interestPaid) {
+		if(interestPaid >= 0) {
+			this.interestPaid = interestPaid;
+		}
+	}
+	
+	
+	public double getPrincipalPaid() {
+		return principalPaid;
+	}
+	public void setPrincipalPaid(double principalPaid) {
+		if(principalPaid >= 0) {
+			this.principalPaid = principalPaid;
+		}
+	}
+	
+	
+	public double getRemainingBalance() {
+		return remainingBalance;
+	}
+	public void setRemainingBalance(double remainingBalance) {
+		if(remainingBalance >= 0) {
+			this.remainingBalance = remainingBalance;
+		}
+	}
+	
+	/*Methods*/
+	
+	
+	
+	
+
+}
