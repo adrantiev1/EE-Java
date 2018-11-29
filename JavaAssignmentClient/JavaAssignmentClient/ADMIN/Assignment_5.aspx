de5e7e5486f4ca556659cb6b61569881fd38338b
adrantiev1 <35266709+adrantiev1@users.noreply.github.com>
2018-11-28 17:01:38 -0700

    Added clinetside .net solution Assignment 5

---
+++ JavaAssignmentClient/JavaAssignmentClient/ADMIN/Assignment_5.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Assignment_5.aspx.cs" Inherits="JavaAssignmentClient.ADMIN.Assignment_5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Assignment 5</h1>
+            <div class="col-md-6">
+                <asp:LinkButton ID="randomNumber" runat="server" OnClick="randomNumber_Click" >Gen RND Number</asp:LinkButton>
+                <asp:Label  ID="randomNumberLAabel"  runat="server" />
+
+                <asp:TextBox ID="amount" runat="server"></asp:TextBox>
+                <asp:LinkButton  ID="genRndArray" runat="server" OnClick="genRndArray_Click" >Gen RND Array</asp:LinkButton>
+                <asp:Label  AssociatedControlID="genRndArray" ID="rndArrayLabel"  runat="server" />
+            </div>
+
+            <div class="col-md-6">
+                <asp:Button ID="getJobs" Text="Get Jobs" runat="server" OnClick="getJobs_Click" />
+                <asp:GridView ID="JobsGridView" runat="server" >
+                  
+                </asp:GridView>
+
+
+                <asp:TextBox ID="jobIdTestBox" runat="server">
+
+                </asp:TextBox>
+                <asp:Button ID="search" runat="server" Text="search for a job" OnClick="search_Click"/>
+
+                <asp:Label ID="jobID" runat="server" Text="Label"></asp:Label>
+                <asp:Label ID="jobTitle" runat="server" Text="Label"></asp:Label>
+                <asp:Label ID="maxSalary" runat="server" Text="Label"></asp:Label>
+                <asp:Label ID="minSalary" runat="server" Text="Label"></asp:Label>
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ JavaAssignmentClient/JavaAssignmentClient/ADMIN/Assignment_5.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JavaAssignmentClient.ADMIN
+{
+    public partial class Assignment_5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void randomNumber_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.HumanResourceXmlWebServiceClient webClient = new ServiceReference1.HumanResourceXmlWebServiceClient();
+
+            randomNumberLAabel.Text =webClient.luckyLottoNumber().ToString();
+        }
+
+        protected void genRndArray_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.HumanResourceXmlWebServiceClient webClient = new ServiceReference1.HumanResourceXmlWebServiceClient();
+            
+            rndArrayLabel.Text = webClient.luckyNumberArray(int.Parse(amount.Text));
+        }
+
+        protected void getJobs_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.HumanResourceXmlWebServiceClient webClient = new ServiceReference1.HumanResourceXmlWebServiceClient();
+            var jobs = webClient.findAllJob();
+
+
+            JobsGridView.DataSource = jobs;
+            JobsGridView.DataBind();
+
+        }
+
+        protected void search_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.HumanResourceXmlWebServiceClient webClient = new ServiceReference1.HumanResourceXmlWebServiceClient();
+
+            var jobDetails = webClient.findOneJob(jobIdTestBox.Text);
+
+            jobID .Text= jobDetails.jobId;
+            jobTitle.Text = jobDetails.jobTitle;
+            maxSalary.Text = jobDetails.maxSalary.ToString();
+            minSalary.Text = jobDetails.minSalary.ToString();
+
+
+
+        }
+    }
+}
